c4c1fba5835835151c5153ffead178e4f92643e3
Anuj Garg <anujgarg.cse@gmail.com>
2016-07-03 15:31:23 +0530

    github ready

--- app/src/main/java/sample/android/bignerdranch/com/simple/MainActivity.java
+++ app/src/main/java/sample/android/bignerdranch/com/simple/MainActivity.java
@@ -2,6 +2,7 @@ package sample.android.bignerdranch.com.simple;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import sample.android.bignerdranch.com.mylibrary.Awesome;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Awesome.getInstance().makeMeAwesome(getApplication(), "Anuj");
     }
 }
